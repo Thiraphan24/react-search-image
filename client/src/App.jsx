1224b00a928f661f9ef92c06b5cc97919d2f3861
Thiraphan24 <thiraphan.codercamp@gmail.com>
2023-10-03 15:32:42 +0700

    feat: add fetch data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Picture from "./component/Picture";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [word, setWord] = useState("");
+  const [photos, setPhotos] = useState([]);
+  const key = "whIGJ7E0UHo907gt-PvQwsPi1XOTXWFwGkHCMNkQl6g";
 
+  function searchImage(e) {
+    e.preventDefault();
+    if (!word) {
+      alert("กรุณาป้อนชื่อรูปภาพ");
+    } else {
+      fetchImageFromAPI();
+    }
+  }
+
+  async function fetchImageFromAPI() {
+    const url = `https://api.unsplash.com/search/photos?page=1&query=${word}&client_id=${key}&per_page=15`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const result = data.results;
+    if (result.length == 0) {
+      alert("ไม่มีข้อมูลรูปภาพ");
+      setWord("");
+    } else {
+      setPhotos(result);
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>ระบบค้นหารูปภาพด้วย API</h1>
+      <form onSubmit={searchImage}>
+        <input
+          type="text"
+          placeholder="ป้อนชื่อรูปภาพที่ต้องการค้นหา"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+        />
+        <button type="submit">ค้นหา</button>
+      </form>
+      <div className="search-result">
+        {photos.map((data, index) => {
+          return <Picture {...data} key={index} />;
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
